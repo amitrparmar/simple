65ad0238aff1361baebcac89b5c7b6648310d607
Amit Parmar <AParmar@rdindia.wsint.com>
2016-10-05 15:21:21 +0530

    Changes 5-oct-16

--- src/test/java/TestLogin.java
+++ src/test/java/TestLogin.java
@@ -1,8 +1,13 @@
+import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+//import Tests.GenLogs;
 import pages.web.LoginPage;
 
 /**
@@ -12,17 +17,20 @@ public class TestLogin
 {
 
     public static WebDriver driver;
+    public static Logger log;
 
-    @BeforeTest
+    @BeforeSuite
     void initialized()
     {
         System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
         driver = new ChromeDriver();
         //driver.get("https://www.hostgator.in/login.php");
         driver.get("https://mail.google.com/mail/");
+        log = Logger.getLogger(this.getClass().getName());
+        
     }
 
-    @AfterTest
+    @AfterSuite
     void tearDown()
     {
         try {
